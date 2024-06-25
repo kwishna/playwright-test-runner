a2873491da4d04bcd60404e3b7c3ef7f421e6e8d
kwishna <kwishna@gmail.com>
2024-06-26 03:01:04 +0530

    paramterized

--- src/tests/parametrized-csv.spec.ts
+++ src/tests/parametrized-csv.spec.ts
@@ -1,15 +1,30 @@
-// import fs from 'fs';
-// import path from 'path';
-// import { test } from '@playwright/test';
-// import { parse } from 'csv-parse/sync';
+import fs from 'fs';
+import path from 'path';
+import { test } from '@playwright/test';
+import { parse } from 'csv-parse/sync';
 
-const records = parse(fs.readFileSync(path.join(__dirname, 'input.csv')), {
+const csvData = fs.readFileSync(path.resolve("./src/test-data/test-data.csv")).toString();
+
+const records = parse(csvData, {
     columns: true,
-    skip_empty_lines: true
+    skip_empty_lines: true,
+    delimiter: ","
 });
+
+/**
+    [
+        { param1: 'TC001', param2: 'fruit', param3: 'apple' },
+        { param1: 'TC002', param2: 'veg', param3: 'tomato' }
+    ]
+*/
   
 for (const record of records) {
-    test(`foo: ${record.test_case}`, async ({ page }) => {
-        console.log(record.test_case, record.some_value, record.some_other_value);
+    test(`foo: ${record.TC_NAME}`, async ({ page }) => {
+        await page.goto('https://www.google.co.in/');
+        await page.getByLabel('Search', { exact: true }).click();
+        await page.getByLabel('Search', { exact: true }).fill(record.TOPIC);
+        await page.getByLabel('Google Search').first().click();
+        await page.getByRole('combobox', { name: 'Search' }).fill(record.DATA);
+        await page.getByRole('button', { name: 'Search', exact: true }).click();
     });
 }
